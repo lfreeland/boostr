5d0f90e33f29ec79ad0ce4e14ef2471c4acd180e
Luke Freeland <LukeJFreeland@gmail.com>
2017-08-05 07:33:56 -0400

    My in-progress work of adding lightning listview row clicking to select or deselect the row. Currently, it clicks the checkbox but then some other code clicks it again making it appear as though it wasn't clicked. Have to fix that.

--- app/js/listviewrowclick.js
+++ app/js/listviewrowclick.js
@@ -1,73 +1,123 @@
 var BfsListViewRowClick = BfsListViewRowClick || {};
 
 BfsListViewRowClick.addClassicMutationObserver = function () {
-  var container = document;
-  var observer = new MutationObserver(BfsListViewRowClick.addRowOnClickCheckboxEventHandlerWhenListViewRowsAdded);
+    var container = document;
+    var observer = new MutationObserver(BfsListViewRowClick.addRowOnClickCheckboxEventHandlerWhenListViewRowsAdded);
 
-  observer.observe(container, {
-    childList: true,
-    attributes: false,
-    characterData: false,
-    subtree: true,
-    attributeOldValue: false,
-    characterDataOldValue: false
-  });
+    observer.observe(container, {
+        childList: true,
+        attributes: false,
+        characterData: false,
+        subtree: true,
+        attributeOldValue: false,
+        characterDataOldValue: false
+    });
+}
+
+BfsListViewRowClick.addLightningMutationObserver = function () {
+    var container = document;
+    var observer = new MutationObserver(BfsListViewRowClick.addLightningRowOnClickCheckboxEventHandlerWhenListViewRowsAdded);
+
+    observer.observe(container, {
+        childList: true,
+        attributes: false,
+        characterData: false,
+        subtree: true,
+        attributeOldValue: false,
+        characterDataOldValue: false
+    });
+}
+
+BfsListViewRowClick.addLightningRowOnClickCheckboxEventHandlerWhenListViewRowsAdded = function (mutations) {
+    // Since this code executes on each DOM addition or removal to the document,
+    // this low-level code was added to be as performant as possible.
+    var mutationsLength = mutations.length;
+    for (var mutationIndex = 0; mutationIndex < mutationsLength; ++mutationIndex) {
+        var mutation = mutations[mutationIndex];
+        if (mutation.addedNodes &&
+            mutation.addedNodes.length > 0) {
+
+            var addedNodesLength = mutation.addedNodes.length;
+
+            for (var addedNodeIndex = 0; addedNodeIndex < addedNodesLength; ++addedNodeIndex) {
+                var addedNode = mutation.addedNodes[addedNodeIndex];
+
+                if (addedNode &&
+                    addedNode.tagName &&
+                    addedNode.tagName == "TH" &&
+                    addedNode.className &&
+                    typeof addedNode.className.indexOf === 'function' &&
+                    addedNode.className.indexOf('actionColumnHeader') != -1) {
+                    debugger;
+                    BfsListViewRowClick.addRowOnClickCheckBoxEventHandlerInLightning();
+                }
+            }
+        }
+    }
 }
 
 BfsListViewRowClick.addRowOnClickCheckboxEventHandlerWhenListViewRowsAdded = function (mutations) {
-  // Since this code executes on each DOM addition or removal to the document,
-  // this low-level code was added to be as performant as possible.
-  var mutationsLength = mutations.length;
-  for (var mutationIndex = 0; mutationIndex < mutationsLength; ++mutationIndex) {
-    var mutation = mutations[mutationIndex];
-    if (mutation.addedNodes &&
-        mutation.addedNodes.length > 0) {
+    // Since this code executes on each DOM addition or removal to the document,
+    // this low-level code was added to be as performant as possible.
+    var mutationsLength = mutations.length;
+    for (var mutationIndex = 0; mutationIndex < mutationsLength; ++mutationIndex) {
+        var mutation = mutations[mutationIndex];
+        if (mutation.addedNodes &&
+            mutation.addedNodes.length > 0) {
 
-      var addedNodesLength = mutation.addedNodes.length;
+            var addedNodesLength = mutation.addedNodes.length;
 
-      for (var addedNodeIndex = 0; addedNodeIndex < addedNodesLength; ++addedNodeIndex) {
-        var addedNode = mutation.addedNodes[addedNodeIndex];
+            for (var addedNodeIndex = 0; addedNodeIndex < addedNodesLength; ++addedNodeIndex) {
+                var addedNode = mutation.addedNodes[addedNodeIndex];
 
-        if (addedNode.className &&
-            (addedNode.className.indexOf('x-grid3-row') != -1 ||
-             addedNode.className.indexOf('dataRow') != -1)) {
-          BfsListViewRowClick.addClassicRowOnClickCheckBoxEventHandler();
+                if (addedNode.className &&
+                    (addedNode.className.indexOf('x-grid3-row') != -1 ||
+                     addedNode.className.indexOf('dataRow') != -1)) {
+                    BfsListViewRowClick.addRowOnClickCheckBoxEventHandlerInClassic();
+                }
+            }
         }
-      }
     }
-  }
 }
 
 
-BfsListViewRowClick.addClassicRowOnClickCheckBoxEventHandler = function () {
-  // Setup Listview Rows
-  $('tr.dataRow').has('td:first-child input[type="checkbox"]')
-    .on('click', BfsListViewRowClick.clickCheckboxFunction);
+BfsListViewRowClick.addRowOnClickCheckBoxEventHandlerInClassic = function () {
+    var setupListViewRows = $('tr.dataRow');
+    var checkboxRows = setupListViewRows.has('td:first-child input[type="checkbox"]');
 
-  // Object Listview Rows
-  $('.listBody table.x-grid3-row-table tr').has('td:first-child input[type="checkbox"]')
-    .on('click', BfsListViewRowClick.clickCheckboxFunction);
+    checkboxRows.on('click', BfsListViewRowClick.clickCheckboxFunction);
+
+    var listviewRows = $('.listBody table.x-grid3-row-table tr');
+    checkboxRows = listviewRows.has('td:first-child input[type="checkbox"]');
+
+    checkboxRows.on('click', BfsListViewRowClick.clickCheckboxFunction);
+}
+
+BfsListViewRowClick.addRowOnClickCheckBoxEventHandlerInLightning = function () {
+    var listviewRows = $('table.slds-table.uiVirtualDataTable tr');
+    checkboxRows = listviewRows.has('td:nth-child(2) input[type="checkbox"]');
+
+    checkboxRows.on('click', BfsListViewRowClick.clickCheckboxFunction);
 }
 
 BfsListViewRowClick.clickCheckboxFunction = function (e) {
-  if (!$(e.target).is('input:checkbox')) {
-      $(this).find('input:checkbox:first').click();
-  }
+    if (!$(e.target).is('input:checkbox')) {
+        $(this).find('input:checkbox:first').click();
+        e.stopImmediatePropagation();
+    }
 }
 
 BfsListViewRowClick.initOnCheckboxRowClickHandlers = function () {
-  var someLightningElement = document.getElementById('auraAppcacheProgress');
+    debugger;
+    var someLightningElement = document.getElementById('auraAppcacheProgress');
 
-  if (!someLightningElement) {
-    // Use mutation observer for listviews dynamically added via AJAX.
-    // Supposedly, .on('click'...) should work with dynamically added content
-    // but it's not working here so mutation observer used instead.
-    BfsListViewRowClick.addClassicMutationObserver();
-
-    // For setup list views that are added directly to the page and not
-    // through AJAX.
-    BfsListViewRowClick.addClassicRowOnClickCheckBoxEventHandler();
-  }
+    if (someLightningElement) {
+        BfsListViewRowClick.addLightningMutationObserver();
+    }
+    else {
+        BfsListViewRowClick.addClassicMutationObserver();
+        BfsListViewRowClick.addRowOnClickCheckBoxEventHandlerInClassic();
+    }
 }
 
 BfsListViewRowClick.initOnCheckboxRowClickHandlers();
